c5d5a47447df46f9b65061d806c239e2554b958a
Miguel Martin <Miguel Martin@LSTKCO14687.softtek.com>
2017-06-05 12:50:26 +0200

    back actualizado

--- server/boot/create-mocks-contactos.js
+++ server/boot/create-mocks-contactos.js
@@ -10,10 +10,16 @@ module.exports = function(app) {
   }
 
   // Arrays
-  var tipo = ['Teléfono', 'Correo', 'Facebook', 'LinkedIn', 'Twitter'];
   var usuarios = ['martin89', 'alonso32', 'tizón_15', 'espinoza45', 'monzooon', 'minguez_32', 'moreno.00', 'ortiz1', '_fernandez'];
   var correos = ['gmail.com', 'outlook.es', 'hootmail.com', 'oll.ru', 'outlook.es', 'google.com'];
-  var tipoContactoGenerado;
+  var tipo = ['Teléfono', 'Correo', 'Facebook', 'LinkedIn', 'Twitter'];
+  var ultimoContactoGenerado = null;
+
+  function resetVariables() {
+    tipo = ['Teléfono', 'Correo', 'Facebook', 'LinkedIn', 'Twitter'];
+    ultimoContactoGenerado = null;
+  }
+  resetVariables();
 
   /**
    * Devuelve un contacto con campos aleatorios
@@ -30,16 +36,26 @@ module.exports = function(app) {
   }
 
   function generarTipo() {
-    return tipoContactoGenerado = tipo[linearGenerator(0, tipo.length)];
+    ultimoContactoGenerado = tipo[linearGenerator(0, tipo.length)];
+    for (var i = 0; i < tipo.length; i++) {
+      if (tipo[i] == ultimoContactoGenerado) {
+        tipo.splice(i, 1);
+      }
+    }
+    return ultimoContactoGenerado;
   }
 
   function generarContactoCondicioanal() {
-    if (tipoContactoGenerado == 'Teléfono')
+    if (ultimoContactoGenerado == 'Teléfono')
       return 600000000 + linearGenerator(0, 99999999);
-    else if (tipoContactoGenerado == 'Correo')
+    else if (ultimoContactoGenerado == 'Correo')
       return usuarios[linearGenerator(0, usuarios.length)] + '@' + correos[linearGenerator(0, correos.length)];
-    else
-      return usuarios[linearGenerator(0, usuarios.length)];
+    else if (ultimoContactoGenerado == 'Facebook')
+      return 'https://www.facebook.com/' + usuarios[linearGenerator(0, usuarios.length)];
+    else if (ultimoContactoGenerado == 'LinkedIn')
+      return 'https://www.linkedin.com/in/' + usuarios[linearGenerator(0, usuarios.length)];
+    else if (ultimoContactoGenerado == 'Twitter')
+      return 'https://twitter.com/' + usuarios[linearGenerator(0, usuarios.length)];
   }
 
   /**
@@ -50,9 +66,11 @@ module.exports = function(app) {
   function generadorContactos(amount) {
     var array = [];
     for (var i = 0; i < amount; i++) {
-      for (var j = 0; j < linearGenerator(1, 4); j++) {
-        array.push(contactoAleatorio(i));
+      var limiteContactos = linearGenerator(1, 5);
+      for (var j = 0; j < limiteContactos; j++) {
+        array.push(contactoAleatorio(i + 1));
       }
+      resetVariables();
     }
     return array;
   }
