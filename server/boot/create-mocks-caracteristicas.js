029fc32a3be5d4e036cee0048a3fb33fcf602a6c
Miguel Martin <Miguel Martin@LSTKCO14687.softtek.com>
2017-06-07 10:56:26 +0200

    back actualizado

--- server/boot/create-mocks-caracteristicas.js
+++ server/boot/create-mocks-caracteristicas.js
@@ -1,55 +1,166 @@
 'use strict';
 module.exports = function(app) {
   const Caracteristica = app.models.Caracteristica;
-  [
-    {
-      nombre: 'javaScript',
-      tipo: 'programacion',
-    },
-    {
-      nombre: 'java',
-      tipo: 'programacion',
-    },
-    {
-      nombre: 'ingles',
-      tipo: 'idioma',
-    },
-    {
-      nombre: 'guardias',
-      tipo: 'disponibilidad',
-    },
-    {
-      nombre: 'frances',
-      tipo: 'idioma',
-    },
-    {
-      nombre: 'aleman',
-      tipo: 'idioma',
-    },
-    {
-      nombre: 'portugues',
-      tipo: 'idioma',
-    },
-    {
-      nombre: 'angular',
-      tipo: 'programacion',
-    },
-    {
-      nombre: 'html',
-      tipo: 'maquetacion',
-    },
-    {
-      nombre: 'css',
-      tipo: 'maquetacion',
-    },
-    {
-      nombre: 'python',
-      tipo: 'programacion',
-    },
-    {
-      nombre: 'viajar',
-      tipo: 'disponibilidad',
-    },
+  [{
+    nombre: 'JavaScript',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Java',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Ingles',
+    tipo: 'idioma',
+  },
+  {
+    nombre: 'Guardias',
+    tipo: 'disponibilidad',
+  },
+  {
+    nombre: 'Frances',
+    tipo: 'idioma',
+  },
+  {
+    nombre: 'Aleman',
+    tipo: 'idioma',
+  },
+  {
+    nombre: 'Portugues',
+    tipo: 'idioma',
+  },
+  {
+    nombre: 'Angular 1',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Angular 2',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'React',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Polymer',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'HTML',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'CSS',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'Python',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Viajar',
+    tipo: 'disponibilidad',
+  },
+  {
+    nombre: 'C',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'C++',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Objective-C',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'C#',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'PHP',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Visual Basic',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Ruby',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Perl',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Bash',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Lisp',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Pascal',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Delphi',
+    tipo: 'programacion',
+  },
+  {
+    nombre: '.NET',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'SQL',
+    tipo: 'base de datos',
+  },
+  {
+    nombre: 'Lua',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Assembly',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Node',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'Bootstrap',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'Sass',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'Less',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'Stylus',
+    tipo: 'maquetacion',
+  },
+  {
+    nombre: 'Oracle',
+    tipo: 'base de datos',
+  },
+  {
+    nombre: 'MySQL',
+    tipo: 'base de datos',
+  },
+  {
+    nombre: 'Android',
+    tipo: 'programacion',
+  },
+  {
+    nombre: 'iOS',
+    tipo: 'programacion',
+  },
   ].map(function(elem) {
     Caracteristica.create(elem);
   });
